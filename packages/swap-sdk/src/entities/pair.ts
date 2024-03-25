858d8f921b306b49c5ff12d60d5691dc238138df
powman <paulophpweb@gmail.com>
2024-03-25 16:52:50 -0300

    Fix: Error

--- packages/swap-sdk/src/entities/pair.ts
+++ packages/swap-sdk/src/entities/pair.ts
@@ -94,7 +94,7 @@ export class Pair {
       tokenAmounts[0].currency.chainId,
       Pair.getAddress(tokenAmounts[0].currency, tokenAmounts[1].currency),
       18,
-        'ICELP',
+        'WHALELP',
         'whalechain.live LP'
     )
     this.tokenAmounts = tokenAmounts as [CurrencyAmount<ERC20Token>, CurrencyAmount<ERC20Token>]
